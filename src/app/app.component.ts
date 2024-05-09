1f7822553c27a4567f401a30ef4712787f650741
Gabriel Pereyra <gabrielp1689@gmail.com>
2024-05-09 09:15:06 -0300

    consume API with HttpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UnoComponent } from './components/uno/uno.component';
+import { CountryComponent } from './components/country/country.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UnoComponent],
+  imports: [RouterOutlet, UnoComponent, CountryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
