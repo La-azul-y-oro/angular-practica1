9863ddffeaaf1de6cd58943406e9a46dc1dc07ef
Estudiantes <gabrielp1689@gmail.com>
2024-05-24 21:41:27 -0300

    Routing practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { UnoComponent } from './components/uno/uno.component';
 import { CountryComponent } from './components/country/country.component';
 import { UserComponent } from './components/user/user.component';
@@ -7,7 +7,7 @@ import { UserComponent } from './components/user/user.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UnoComponent, CountryComponent, UserComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, UnoComponent, CountryComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { DosComponent } from './components/dos/dos.component';
+import { UnoComponent } from './components/uno/uno.component';
+import { UserComponent } from './components/user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'uno', component: UnoComponent},
+    {path: 'dos', component: DosComponent},
+    {path: '**', component: UserComponent}
+];
