53637e10a8d103efd998b50f14896a1c4d17b9e1
gabopereyra <gabrielp1689@gmail.com>
2024-06-07 21:48:02 -0300

    Add register

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
+  constructor (private fb : FormBuilder){}
+
+  userForm : FormGroup = this.fb.group({
+    username: ['', Validators.required, Validators.email],
+    firstname: ['', Validators.required],
+    lastname: ['', Validators.required],
+    password: ['', Validators.required, Validators.minLength(8)]
+  })
+
+  register(){
+    //implementar metodo login
+  }
+
+  showMsgError(nameField : string){
+    let field = this.userForm.get(nameField); 
+    return (field?.dirty || field?.touched) && field?.invalid;
+  }
 }
